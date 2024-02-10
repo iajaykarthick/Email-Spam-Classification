8a57ead2e459a2d3650ece231bfbd8d403e9298c
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-02-10 12:05:22 -0500

    Add load_dataset.py to handle loading and extracting feature names from spambase dataset

---
+++ src/data/load_dataset.py
@@ -0,0 +1,29 @@
+import re
+import os
+import numpy as np
+from src.config import RAW_DATA_DIR
+
+
+def load_spambase():
+    """
+    Load the spambase dataset from the data/raw folder
+    """
+    spambase = os.path.join(RAW_DATA_DIR, 'spambase.data')
+    data = np.loadtxt(spambase, delimiter=',')
+    X = data[:, :-1]
+    y = data[:, -1]
+    return X, y
+
+
+def extract_feature_names():
+    """
+    Extract the feature names from the spambase dataset
+    """
+    with open(os.path.join(RAW_DATA_DIR, 'spambase.names'), 'r') as file:
+        # read all lines at once
+        lines = file.readlines()
+        
+        # extract feature names 
+        feature_names = [re.sub(r':.*', '', line).strip() for line in lines if re.match(r'^[^|].*:.*', line)] 
+
+    return feature_names
