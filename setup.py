ac12e6a9e76c2605c6abdb520ecc83491b0181aa
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-02-10 12:03:44 -0500

    Add setup.py file for email spam classifier

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='email_spam_classifier',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classification Model for Email Spam',
+    author='Ajay Karthick',
+    license='MIT',
+)
