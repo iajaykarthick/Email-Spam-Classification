2ca0c9e18e9d8844a34b8edb2b99a0648af245dd
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-02-13 07:50:50 -0500

    Add run_experiment function to execute experiments and save plots

--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -1,5 +1,20 @@
+import os
+
+from src.config import EXPERIMENT_DIR
 from .experiment import Experiment
+from .experiment_run import ExperimentRun
 
 
-def create_experiment(name, config, model, optimizer, loss, metrics, callbacks, train_loader, val_loader, test_loader):
-    return Experiment(name, config, model, optimizer, loss, metrics, callbacks, train_loader, val_loader, test_loader)
+def run_experiment(experiment_id: str):
+    """Run an experiment and return the results"""
+    exp_dir_path = os.path.join(EXPERIMENT_DIR, experiment_id)
+
+    if not os.path.exists(exp_dir_path):
+        raise FileNotFoundError(f'Experiment directory not found: {exp_dir_path}')
+
+    exp_run = ExperimentRun(
+        experiment_dir=exp_dir_path
+    )
+    exp_run.execute()
+    exp_run.save_plots()
+    print(f'Experiment {experiment_id} completed successfully!')
