71345c58f78ae2de181c582bc7fbae1b816d5ce1
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-02-13 07:12:14 -0500

    Refactor ClassificationMetrics class to allow for more flexible initialization and evaluation

--- src/evaluation/classification_metrics.py
+++ src/evaluation/classification_metrics.py
@@ -1,12 +1,27 @@
 import numpy as np
 
+from sklearn.metrics import roc_curve, auc
+
+
 class ClassificationMetrics:
-    def __init__(self, y_true, y_pred, round_digits=None, exp_id=None):
+    def __init__(self, y_true=None, y_pred=None, y_pred_proba=None, round_digits=None, exp_id=None, **kwargs):
+        if y_true is None and y_pred is None and exp_id is None:
+            raise ValueError('At least one of y_true, y_pred or exp_id must be provided')
         self.y_true = y_true
         self.y_pred = y_pred
+        self.y_pred_proba = y_pred_proba
         self.exp_id = exp_id
         self.round_digits = round_digits
         
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        
+    @staticmethod
+    def from_results(exp_id, results, round_digits=None):
+        class_metrics = ClassificationMetrics(exp_id=exp_id, round_digits=round_digits, **results)
+        return class_metrics
+        
+    
     def _round(self, value):
         if self.round_digits is not None:
             return round(value, self.round_digits)
@@ -46,15 +61,12 @@ class ClassificationMetrics:
         }
 
     def _roc_curve(self):
-        tpr = self._recall()
-        fp = np.sum((self.y_true == 0) & (self.y_pred == 1))
-        tn = np.sum((self.y_true == 0) & (self.y_pred == 0))
-        fpr = self._round(fp / (fp + tn) if fp + tn > 0 else 0)
-        return fpr, self._round(tpr)
+        fpr, tpr, _ = roc_curve(self.y_true, self.y_pred_proba)
+        return fpr, tpr
 
     def _auc(self):
         fpr, tpr = self._roc_curve()
-        return self._round(np.trapz([tpr], [fpr]))
+        return self._round(auc(fpr, tpr))
 
     def _precision_recall_curve(self):
         return self._round(self._recall()), self._round(self._precision())
@@ -75,10 +87,10 @@ class ClassificationMetrics:
         self.precision_recall_curve = self._precision_recall_curve()
         self.average_precision = self._average_precision()
     
-    def evaluate(self, return_metrics=False):
+    def evaluate(self, return_metrics=False, select_metrics=None):
         self._calculate_metrics()
         if return_metrics:
-            return {
+            results = {
                 'accuracy': self.accuracy,
                 'precision': self.precision,
                 'recall': self.recall,
@@ -90,3 +102,7 @@ class ClassificationMetrics:
                 'precision_recall_curve': self.precision_recall_curve,
                 'average_precision': self.average_precision
             }
+            if select_metrics is not None:
+                return {k: v for k, v in results.items() if k in select_metrics}
+            
+            return results
